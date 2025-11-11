8d1720fa882068a7f3e06490339c6838de11d41c
Samruddhi <bedagesamruddhi@gmail.com>
2025-11-11 21:41:11 +0530

    all changes and progress saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,144 @@
-import { useState } from 'react'
-import { useEffect } from 'react'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Search from './components/search.jsx'
+import heroImage from './assets/hero1.png'
+import Spinner from './components/Spinner.jsx'
+import MovieCard from './components/MovieCard.jsx'
+import{ useDebounce} from 'react-use'
+import{ updateSearchCount } from './appwrite.js'
+import {getTrendingMovies } from './appwrite.js';
 
 
-const Card = ({title}) =>{
-  const[count, setCount]=useState(0);
-  const[hasLiked, setHasLiked]= useState(false);
+//API - Application Programming Interface - a set of rules that allows one software application to talk to another
 
-  useEffect(() => {
-    console.log(`${title} has been liked: ${hasLiked}`);
-  }, [hasLiked]);
+const API_BASE_URL ='https://api.themoviedb.org/3';
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
 
-  useEffect(()=>{
-    console.log('CARD RENDERED')
-  }, []);
-  
-  return(
-    <div className="card" onClick={()=> setCount(count+1)}>
-    <h2>{title} <br></br> {count ? count : null}</h2> 
-    
-    <button onClick={() => setHasLiked(!hasLiked)}>
-      {hasLiked ? 'Liked': 'Like'}
-    </button>
-    </div>
-
-  )
+const API_OPTIONS ={
+  method: 'GET',
+  headers:{
+    accept:'application/json',
+    Authorization:`Bearer ${API_KEY}`
+  }
 }
 const App = () => {
 
-    return(
-    <div className="card-container">
-    
-    <Card title="Star wars" rating={5} isCool={true} actors={[{name: 'Actors'}]} />
-    <Card title="Avatar"/>
-    <Card title="The Lion King"/>
-    </div>
-  )
+  const[debouncedSearchTerm, setdebouncedSearchTerm] = useState('')
+  const[searchTerm, setSearchTerm] = useState('')
+
+  const[movieList,setMovieList] = useState([])
+  const[trendingMovies, setTrendingMovies] = useState([])
+
+  const[errorMessage, setErrorMessage ] = useState('')
+  const[isLoading, setIsLoading] = useState(false)
   
+// Debounce the search term to prevent making too many API requests
+// by waiting for the user to stop typing for 500ms
+
+useDebounce(() => {
+    setdebouncedSearchTerm(searchTerm);
+  }, 500, [searchTerm]);
+
+  const fetchMovies = async (query = '') => {
+
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try{
+      const endpoint =  query
+      ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+      :`${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok){
+        throw new Error('Failed to fetch movies')
+      }
+
+      const data = await response.json();
+
+      if(data.response == 'False'){
+        setErrorMessage(data.error || 'Failed to fetch movies')
+        setMovieList([])
+        return;
+      }
+
+      setMovieList(data.results || [] )
+      if(query && data.results.length > 0){
+        await updateSearchCount(query, data.results[0])
+      }
+
+    }catch(error){
+      console.log(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again later.')
+    }finally{
+      setIsLoading(false);
+    }
+  }
+
+  const loadTrendingMovies = async () => {
+  try{
+      const movies = await getTrendingMovies();
+      setTrendingMovies(movies);
+    }catch(error){
+    console.error(`Error fetching trending movies : ${error}`)
+  }
 }
+
+  useEffect(() => {  //only load at the start
+    fetchMovies(debouncedSearchTerm);
+  },[debouncedSearchTerm])
+
+  useEffect( () => {
+    loadTrendingMovies();
+  }, [])
+
+  
+  return(
+    <main>
+      <div className="pattern"></div>
+
+      <div className="wrapper"></div>
+
+      <header>
+        <img src={heroImage} alt="Hero Banner"/>
+        <h1>Find <span className="text-gradient"> Movies </span> You'll Enjoy Without the Hassle</h1>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+      </header>
+      
+      {trendingMovies.length > 0 && (
+        <section className ="trending">
+          <h2 className="trending">Trending Movies</h2>
+
+          <ul>
+            {trendingMovies.map((movie, index) => (
+              <li key={movie.$id}>
+                <p>{index + 1}</p>
+                <img src = {movie.poster_url} alt={movie.title} />
+              </li>
+            ))}
+          </ul>
+        </section>
+      )}
+
+      <section className="all-movies mt-10 px-5">
+  <h2>All Movies</h2>
+
+  {isLoading ? (
+    <Spinner />
+  ) : errorMessage ? (
+    <p className="text-red-500">{errorMessage}</p>
+  ) : (
+    <ul>
+      {movieList.map((movie) => (
+        <li key={movie.id}>
+          <MovieCard movie={movie} />
+        </li>
+      ))}
+    </ul>
+  )}
+</section>
+
+  </main>
+)}
 
 export default App;
---
+++ src/components/search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import searchImage from '../assets/search.png'
+
+const Search = ({searchTerm, setSearchTerm}) => {
+    return(
+        <div className ="search">
+            <div>
+                <img src={searchImage} alt="search"/>
+                <input
+                    type="text"
+                    placeholder="Search through thousands of movies"
+                    value={searchTerm}
+                    onChange={(event) =>setSearchTerm(event.target.value)}
+                    />
+                </div>
+        </div>
+    )
+}
+
+export default Search
